38f5320e5825f98ea6f7620434c2552e931bfd2b
Online-Educational sysytemgit config --global user.name Online-Educational <tiwalkarashwini@gmail.com>
2025-05-01 23:56:07 +0530

    create skills file

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Social from "./components/Social/Social";
 import Navbar from "./components/NavBar/NavBar";
 import { useState } from "react";
 import Preloader from "./components/Preloader/Preloader";
+import Skills from "./components/skills";
+import Experince from './components/Experience/index'
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -19,6 +21,8 @@ function App() {
           <Navbar />
           <Social />
           <Landing />
+          <Skills/>
+          {/* <Experince/> */}
           <ProjectList />
           <About />
           <Contact />
