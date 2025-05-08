b11bed5adf6496ddc590b01c6388e307a2db6eb4
Online-Educational sysytemgit config --global user.name Online-Educational <tiwalkarashwini@gmail.com>
2025-05-08 23:56:28 +0530

    link the social media icons

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box, Typography, Link, Stack, IconButton, Grid } from '@mui/material';
 import { Facebook, Twitter, LinkedIn, Instagram, YouTube } from '@mui/icons-material';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Contact = () => {
   return (
@@ -32,7 +33,7 @@ const Contact = () => {
 
         {/* Nav Links */}
         <Grid container spacing={3} justifyContent="center" sx={{ mb: 4 }}>
-          {['About', 'Skills', 'Experience', 'Projects', 'Education'].map((text) => (
+          {['About', 'Skills', 'Experience', 'My Projects', 'Education'].map((text) => (
             <Grid item key={text}>
               <Link
                 href={`#${text.toLowerCase()}`}
@@ -53,7 +54,7 @@ const Contact = () => {
           <IconButton href="https://twitter.com" target="_blank" sx={{ color: 'white', '&:hover': { color: '#1DA1F2' } }}>
             <Twitter />
           </IconButton>
-          <IconButton href="https://linkedin.com" target="_blank" sx={{ color: 'white', '&:hover': { color: '#0A66C2' } }}>
+          <IconButton href="https://linkedin.com" target="https://www.linkedin.com/in/ashwini-tiwalkar-443550212/" sx={{ color: 'white', '&:hover': { color: '#0A66C2' } }}>
             <LinkedIn />
           </IconButton>
           <IconButton href="https://instagram.com" target="_blank" sx={{ color: 'white', '&:hover': { color: '#E1306C' } }}>
@@ -62,6 +63,7 @@ const Contact = () => {
           <IconButton href="https://youtube.com" target="_blank" sx={{ color: 'white', '&:hover': { color: '#FF0000' } }}>
             <YouTube />
           </IconButton>
+          <a href="https://github.com/ashuzasd" target="https://github.com/ashuzasd" rel="noopener noreferrer"><GitHubIcon style={{ color: 'white' }} /></a>
         </Stack>
 
         {/* Copyright */}
