f1d5491b43b188fc88f468051c647543c6ac0821
Online-Educational sysytemgit config --global user.name Online-Educational <tiwalkarashwini@gmail.com>
2025-05-09 23:15:54 +0530

    add some new things

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -52,7 +52,7 @@ const Landing = () => {
         </div>
       </div>
       
-      <Link to="project" spy={true} smooth={true} offset={-30} duration={500}>
+      <Link to="skills" spy={true} smooth={true} offset={-30} duration={500}>
         <Mouse />
       </Link>
     </div>
